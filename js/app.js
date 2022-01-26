d09a024be448c1fa2c315301c5774cf01f2a2b3d
AlexEastwood <76944389+AlexEastwood@users.noreply.github.com>
2022-01-26 12:36:07 +0000

    Display Names

--- js/app.js
+++ js/app.js
@@ -1,6 +1,32 @@
-fetch('https://randomuser.me/api/?results=12')
-  .then(response => response.json())
-  .then(data => console.log(data));
+function fetchData(url) {
+    return fetch(url)
+            .then(response => response.json())
+}
 
+fetchData('https://randomuser.me/api/?results=3') 
+    .then(data => createCard(data.results))
+
+
+function createCard(data) {
+    body = document.getElementById("images");
+    data.forEach(e => {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = e.name.first;
+        body.appendChild(card)
+    })
+    
+}
+
+
+
+function imageLinks(data) {
+    data.forEach(element => {
+        let img = document.createElement("img");
+        img.src = element.picture.large;
+        let div = document.getElementById("images");
+        div.appendChild(img);
+    });
+}
 
 
